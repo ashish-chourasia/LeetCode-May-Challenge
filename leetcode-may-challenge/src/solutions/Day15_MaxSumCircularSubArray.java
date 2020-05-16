85c4ccb43646628798ecd6e7fe354ea5c80f7a65
Ashish Chourasia <ashish.chourasia1996@googlemail.com>
2020-05-16 17:54:52 +0530

    solution for day15

---
+++ leetcode-may-challenge/src/solutions/Day15_MaxSumCircularSubArray.java
@@ -0,0 +1,33 @@
+package solutions;
+
+public class Day15_MaxSumCircularSubArray {
+
+	public static int maxSubarraySumCircular(int[] arr) {
+		int maxSoFar = Integer.MIN_VALUE, maxEndingHere = 0;
+		int minSoFar = Integer.MAX_VALUE, minEndingHere = 0;
+		int total = 0, ans = 0;
+		for (int i = 0; i < arr.length; i++) {
+			maxEndingHere = Math.max(maxEndingHere + arr[i], arr[i]);
+			maxSoFar = Math.max(maxEndingHere, maxSoFar);
+
+			minEndingHere = Math.min(minEndingHere + arr[i], arr[i]);
+			minSoFar = Math.min(minEndingHere, minSoFar);
+			total = total + arr[i];
+		}
+		if (maxSoFar > 0) {
+			ans = Math.max(maxSoFar, total - minSoFar);
+		} else {
+			ans = maxSoFar;
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 5, -3, 5 };
+		System.out.println(maxSubarraySumCircular(arr));
+	}
+	/*
+	 * Input: [3,-2,2,-3] Output: 3 Explanation: Subarray [3] and [3,-2,2] both have
+	 * maximum sum 3
+	 */
+}
